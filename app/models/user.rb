8363340c921b40447fbb73ac5419247e8ff342e0
Andre Correa <andre.murillo.correa@gmail.com>
2018-10-03 17:20:48 -0300

    Add form validation for creating user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    validates :name, :email, :document, :pwd, :age, :height, :weight, :category, :description, presence: true
+
+    validates :email, uniqueness: true
+
+    validates :pwd, length: {:within => 8..30}
 end
