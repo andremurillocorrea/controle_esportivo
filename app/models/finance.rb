80717680ed68fa48d42738fd6114bfe7c104fafa
DanielCozzubo <daniel.mcozzubo@gmail.com>
2018-10-31 18:13:59 -0200

    total dos dividas

--- app/models/finance.rb
+++ app/models/finance.rb
@@ -2,4 +2,14 @@ class Finance < ApplicationRecord
     validates :debt, :value, :status, presence: true
 
     validates :value, numericality: {greater_than_or_equal_to: 0.00}
+
+    def total=(val)
+        @total = val
+    end
+
+
+    def total
+        @total
+    end
+
 end
