cec894925540cfc89aa9571d6fd1bb5d299faaa3
Marcos Tsuzuki <mtsuzuki@usp.br>
2018-10-31 18:25:11 -0200

    -a

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_03_202323) do
+ActiveRecord::Schema.define(version: 2018_10_29_183047) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2018_10_03_202323) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "role"
+    t.string "extension"
   end
 
 end
